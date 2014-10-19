a07ecbc5c09663b8b208a148c503826e48ecbac1
Francisco Olmedo Valencia <f.olmedo.v@gmail.com>
2014-10-19 14:36:58 -0300

    Creada la pagina de login y su css

--- src/Oxigeno/LoginBundle/Controller/DefaultController.php
+++ src/Oxigeno/LoginBundle/Controller/DefaultController.php
@@ -3,11 +3,11 @@
 namespace Oxigeno\LoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('LoginBundle:Default:index.html.twig', array('name' => $name));
+    public function loginAction() {
+        return $this->render('LoginBundle:Default:login.html.twig');
     }
 }
